aeac315de7481832309dd23bc56284ac61d760a0
Dragan <iliya5376@mail.com>
2017-04-24 13:33:02 +0300

    feature(modals.ejs) add checking of null-show property

--- public/js/big-search-viewing.js
+++ public/js/big-search-viewing.js
@@ -1,5 +1,18 @@
+var viewProperties = {
+	null: true
+};
+
+$(document).ready(function() {
+	$('.properties-null-option input').on('click', function() {
+		viewProperties.null = Boolean(Number($(this).val()));
+	})
+});
+
 function view(data) {
-  var source = $("#view-template").html();
-  var template = Handlebars.compile(source);
-  $("#results").html(template(data));
+	var source = $("#view-template").html();
+	var template = Handlebars.compile(source);
+	$("#results").html(template({
+		data: data,
+		properties: viewProperties
+	}));
 }
