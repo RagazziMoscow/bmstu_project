c70106637e1468a32d4331fb807df78a02eff195
Dragan <iliya5376@mail.com>
2017-05-10 13:42:56 +0300

    style(templates.ejs) add buttons for export in xml and json formats

--- public/js/big-search-viewing.js
+++ public/js/big-search-viewing.js
@@ -25,4 +25,18 @@ function view(data) {
     data: data,
     properties: viewProperties
   }));
+
+  // вешаем загрузку файлов на кнопки экспорта
+  $("#export-xml").on("click", function() {
+    var file = new File(["Hello, world!"], "hello world.txt", {
+      type: "text/plain;charset=utf-8"
+    });
+    saveAs(file);
+  });
+  $("#export-json").on("click", function() {
+    var file = new File(["Hello, world!"], "hello world.txt", {
+      type: "text/plain;charset=utf-8"
+    });
+    saveAs(file);
+  });
 }
