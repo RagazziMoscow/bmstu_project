8c6410e2b07926935e0da771a4d7d3f01355ec32
Dragan <iliya5376@mail.com>
2017-05-14 17:49:54 +0300

    style(columns.ejs) add -> to columns names

--- public/js/columns.js
+++ public/js/columns.js
@@ -18,4 +18,19 @@ $(document).ready(function() {
     $(".columns-info__list__item__column-info input:checked").prop("checked", false);
   });
 
+  $(".columns-info__list__item__column-info").each(function(item) {
+    var columnName = $(this).find(".columns-info__list__item__column-info__name");
+    var words = columnName.text().trim().split(".");
+    var div = $(this).find(".columns-info__list__item__column-info__name");
+    if (words.length != 1) {
+      div.find("span").first().remove();
+      words.forEach(function(wordItem) {
+        div.append("<span class='column-name-span'>" + wordItem + "</span>");
+        //alert(wordItem);
+
+      });
+      //words.forEach(function(w))
+    }
+  });
+
 });
